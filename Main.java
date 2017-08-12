86f858688b0eb02d8040a98e7e0aeb52a93918fb
joshuacassidy <Joshcassidy79@gmail.com>
2017-08-12 22:40:52 +0100

    Transactions, Branches and Customers now being written to a file and being used from the file at run time

--- Main.java
+++ Main.java
@@ -4,23 +4,23 @@ public class Main {
     public static void main(String[] args) throws IOException{
         Bank bank = new Bank("Irish Bank");
 
-        try(BufferedReader locFile = new BufferedReader(new FileReader("src/Customers.txt"));
-            BufferedReader dirFile = new BufferedReader(new FileReader("src/CustomersTransactions.txt"))) {
-            String addCustomersData,addCustomersTransactionsData;
-            addCustomersData = locFile.readLine();
-            addCustomersTransactionsData = dirFile.readLine();
+        try(BufferedReader customerFile = new BufferedReader(new FileReader("src/Customers.txt"));
+            BufferedReader transactions = new BufferedReader(new FileReader("src/Transactions.txt"))) {
+            String customersData,transactionsData;
+            customersData = customerFile.readLine();
+            transactionsData = transactions.readLine();
             while(true) {
-                if(addCustomersData != null){
-                    String[] data = addCustomersData.split(",");
+                if(customersData != null){
+                    String[] data = customersData.split(",");
                     bank.addBranch(data[0]);
                     bank.addCustomer(data[0], data[1], Double.parseDouble(data[2]));
-                    addCustomersData = locFile.readLine();
+                    customersData = customerFile.readLine();
                 }
-                else if(addCustomersTransactionsData != null) {
-                    String[] data2 = addCustomersTransactionsData.split(",");
+                else if(transactionsData != null) {
+                    String[] data = transactionsData.split(",");
 
-                    bank.addCustomerTransaction(data2[0], data2[1], Double.parseDouble(data2[2]));
-                    addCustomersTransactionsData = dirFile.readLine();
+                    bank.addCustomerTransaction(data[0], data[1], Double.parseDouble(data[2]));
+                    transactionsData = transactions.readLine();
                 }
                 else {
                     break;
@@ -29,20 +29,20 @@ public class Main {
 
         }
 
-        String addCustomers[][] = { {"Dublin","Dublin","Dublin","Kerry"},{"Harry","Henry","Harvey","Bob"},{"50.05","175.34","220.12","220.12"} };
+        String customers[][] = { {"Dublin","Dublin","Dublin","Kerry"},{"Harry","Henry","Harvey","Bob"},{"50.05","175.34","220.12","220.12"} };
         String customerTransactions[][] = { {"Dublin","Dublin","Kerry"},{"Harry","Henry","Bob"},{"44.22","1.65","44.22"} };
 
         for(Branch county: bank.getBranches()) {
             bank.listCustomers(county.getName(), true);
         }
 
-        try(FileWriter locFile = new FileWriter("src/Customers.txt");
-            FileWriter dirFile = new FileWriter("src/CustomersTransactions.txt")) {
-            for(int i = 0; i< addCustomers[0].length; i++) {
-                locFile.write(String.format("%s, %s, %s\n",addCustomers[0][i],addCustomers[1][i],addCustomers[2][i]));
+        try(FileWriter customerFile = new FileWriter("src/Customers.txt");
+            FileWriter TransactionsFile = new FileWriter("src/Transactions.txt")) {
+            for(int i = 0; i< customers[0].length; i++) {
+                customerFile.write(String.format("%s, %s, %s\n",customers[0][i],customers[1][i],customers[2][i]));
             }
             for(int i = 0; i< customerTransactions[0].length; i++) {
-                dirFile.write(String.format("%s, %s, %s\n",customerTransactions[0][i], customerTransactions[1][i], customerTransactions[2][i]));
+                TransactionsFile.write(String.format("%s, %s, %s\n",customerTransactions[0][i], customerTransactions[1][i], customerTransactions[2][i]));
             }
         }
 
